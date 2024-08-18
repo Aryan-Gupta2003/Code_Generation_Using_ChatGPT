94bfa7f9bd19b532f131a6201626af4ad89bfc88
Aryan Gupta <aryang.gzb@gmail.com>
2024-08-18 19:47:25 +0530

    Added new file

---
+++ frontend/src/components/GeneratedCode.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GeneratedCode({ code }) {
+  return (
+    <div>
+      <h2>Generated Code</h2>
+      <pre>{code}</pre>
+    </div>
+  );
+}
+
+export default GeneratedCode;
